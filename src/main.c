3f0f383fc63568eb8338ccfb2769b8977161bb0b
John Reynolds <felixinutero@gmail.com>
2022-02-27 16:59:52 +0800

    cleaning up

--- src/main.c
+++ src/main.c
@@ -1,20 +1,20 @@
-#include "../lib/perceptron.h"
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+#include "../lib/perceptron.h"
+
 /*
- * Follow the amazing adventures of our intrepid Binary Classifier as it
- * attempts to learn and perform the logical OR and AND functions!
+ * Follow the amazing adventures of our intrepid Binary Classifier as it attempts to learn and
+ * perform the logical OR and AND functions!
  *
  * Author:  John Reynolds
  * Version: 16.01.22
  */
 
 int main() {
-
   srand(time(NULL));
 
   const uint8_t num_weights = 2;
@@ -29,24 +29,17 @@ int main() {
   register uint8_t i = 0, j = 0;
 
   for (i = 0; i < 2; i++) {
-
-    bias = 0.0;
-    total_error = 0.0;
+    bias = 0.0, total_error = 0.0;
 
     if (i < 1) {
-
-      puts("\n\nHi there! I'm a Perceptron. Watch me learn the logical OR "
-           "function!\n");
-
+      puts("\n\nHi there! I'm a Perceptron. Watch me learn the logical OR function!\n");
       training_sets[0] = TrainingSet_create(0, 0, 0);
       training_sets[1] = TrainingSet_create(0, 1, 1);
       training_sets[2] = TrainingSet_create(1, 0, 1);
       training_sets[3] = TrainingSet_create(1, 1, 1);
 
     } else {
-
       puts("\nNOW watch me learn the logical AND function!\n");
-
       training_sets[0] = TrainingSet_create(0, 0, 0);
       training_sets[1] = TrainingSet_create(0, 1, 0);
       training_sets[2] = TrainingSet_create(1, 0, 0);
@@ -54,23 +47,18 @@ int main() {
     }
 
     puts("Here's the training set, i.e. the thing I'm trying to learn:\n");
-
     for (j = 0; j < num_training_sets; j++) {
       TrainingSet_print(training_sets[j]);
     }
 
     puts("\n\nNow for the learning part... Wish me luck!\n");
-
-    train(num_training_sets, training_sets, num_weights, weights, &bias,
-          &total_error, num_epochs);
+    train(num_training_sets, training_sets, num_weights, weights, &bias, &total_error, num_epochs);
 
     puts("\nAnd here's where I show what I've learnt:\n");
-
     printf("\tTest [0 0] %d\n", calc_output(num_weights, weights, 0, 0, &bias));
     printf("\tTest [0 1] %d\n", calc_output(num_weights, weights, 0, 1, &bias));
     printf("\tTest [1 0] %d\n", calc_output(num_weights, weights, 1, 0, &bias));
-    printf("\tTest [1 1] %d\n\n",
-           calc_output(num_weights, weights, 1, 1, &bias));
+    printf("\tTest [1 1] %d\n\n", calc_output(num_weights, weights, 1, 1, &bias));
 
     for (j = 0; j < num_training_sets; j++) {
       TrainingSet_destroy(training_sets[j]);
--- src/perceptron.c
+++ src/perceptron.c
@@ -1,4 +1,5 @@
 #include "../lib/perceptron.h"
+
 #include <assert.h>
 #include <math.h>
 #include <stdint.h>
@@ -7,49 +8,36 @@
 #include <string.h>
 
 TrainingSet *TrainingSet_create(int input_1, int input_2, int output) {
-
   TrainingSet *training_set = malloc(sizeof(TrainingSet));
   assert(training_set != NULL);
 
   training_set->inputs[0] = input_1;
   training_set->inputs[1] = input_2;
-
   training_set->output = output;
 
   return training_set;
 }
 
 void initialise_weights(int num_weights, double *weights, double *bias) {
-
   assert(weights != NULL && bias != NULL);
-
   for (register uint8_t i = 0; i < num_weights; i++) {
     weights[i] = (double)rand() / RAND_MAX * 2.0 - 1.0;
   }
-
   *bias = (double)rand() / RAND_MAX * 2.0 - 1.0;
 }
 
-void train(int num_training_sets, TrainingSet **training_sets, int num_weights,
-           double *weights, double *bias, double *total_error, int num_epochs) {
+void train(int num_training_sets, TrainingSet **training_sets, int num_weights, double *weights,
+           double *bias, double *total_error, int num_epochs) {
+  assert(training_sets != NULL && weights != NULL && bias != NULL && total_error != NULL);
 
-  assert(training_sets != NULL && weights != NULL && bias != NULL &&
-         total_error != NULL);
-
-  puts("I start out by setting my weights and bias to random numbers between "
-       "-1 and 1:\n");
-
+  puts("I start out by setting my weights and bias to random numbers between -1 and 1:\n");
   initialise_weights(num_weights, weights, bias);
-
-  printf("\tWeights: %9f  %9f\tBias: %9f\n\nThen the training begins...\n\n",
-         weights[0], weights[1], *bias);
+  printf("\tWeights: %9f  %9f\tBias: %9f\n\nThen the training begins...\n\n", weights[0],
+         weights[1], *bias);
 
   for (register uint8_t i = 0; i < num_epochs; i++) {
-
     *total_error = 0;
-
     printf("Epoch %d:\n", i + 1);
-
     for (register uint8_t j = 0; j < num_training_sets; j++) {
       update_weights(training_sets, num_weights, weights, bias, total_error, j);
       printf("\tWeights: %9f  %9f\tBias: %9f\n", weights[0], weights[1], *bias);
@@ -58,13 +46,10 @@ void train(int num_training_sets, TrainingSet **training_sets, int num_weights,
   }
 }
 
-double calc_dot_product_bias(int num_weights, double *weights, double *inputs,
-                             double *bias) {
-
+double calc_dot_product_bias(int num_weights, double *weights, double *inputs, double *bias) {
   assert(weights != NULL && inputs != NULL && bias != NULL);
 
   double dot_product_bias = 0;
-
   for (register uint8_t i = 0; i < num_weights; i++) {
     dot_product_bias = dot_product_bias + (weights[i] * inputs[i]);
   }
@@ -73,29 +58,22 @@ double calc_dot_product_bias(int num_weights, double *weights, double *inputs,
   return dot_product_bias;
 }
 
-int calc_output(int num_weights, double *weights, double input_1,
-                double input_2, double *bias) {
-
+int calc_output(int num_weights, double *weights, double input_1, double input_2, double *bias) {
   assert(weights != NULL && bias != NULL);
 
   double inputs[] = {input_1, input_2};
-
-  double dot_product_bias =
-      calc_dot_product_bias(num_weights, weights, inputs, bias);
+  double dot_product_bias = calc_dot_product_bias(num_weights, weights, inputs, bias);
 
   return (dot_product_bias > 0) ? 1 : 0;
 }
 
-void update_weights(TrainingSet **training_sets, int num_weights,
-                    double *weights, double *bias, double *total_error, int j) {
+void update_weights(TrainingSet **training_sets, int num_weights, double *weights, double *bias,
+                    double *total_error, int j) {
+  assert(training_sets != NULL && weights != NULL && bias != NULL && total_error != NULL);
 
-  assert(training_sets != NULL && weights != NULL && bias != NULL &&
-         total_error != NULL);
-
-  double error = training_sets[j]->output -
-                 calc_output(num_weights, weights, training_sets[j]->inputs[0],
-                             training_sets[j]->inputs[1], bias);
-
+  double error =
+      training_sets[j]->output - calc_output(num_weights, weights, training_sets[j]->inputs[0],
+                                             training_sets[j]->inputs[1], bias);
   *total_error = *total_error + fabs(error);
 
   for (register uint8_t i = 0; i < num_weights; i++) {
@@ -106,8 +84,7 @@ void update_weights(TrainingSet **training_sets, int num_weights,
 
 void TrainingSet_print(TrainingSet *training_set) {
   assert(training_set != NULL);
-  printf("\t[%d %d] %d\n", training_set->inputs[0], training_set->inputs[1],
-         training_set->output);
+  printf("\t[%d %d] %d\n", training_set->inputs[0], training_set->inputs[1], training_set->output);
 }
 
 void TrainingSet_destroy(TrainingSet *training_set) {
